01331bb2c3d57ed0afd437babcf2fc37b87823ac
Tom Symons <tom@symonshouse.com>
2024-05-01 21:01:16 -0500

    moved enable_port

--- run_i2c.py
+++ run_i2c.py
@@ -324,7 +324,7 @@ def setup_port(port):
   else:
       i2c = SMBus(4)
       en_pin = SC_EN_PIN
-  print(f'Setup port=%s', port)
+  print(f'Setup port={port}')
   tic = TicI2C(i2c, dev_addr, en_pin)
 
 # Configure GPIO pins
@@ -437,10 +437,11 @@ if opts.ssa is not None:
     start = int(args[3]) if len(args) > 3 else 4000
     print(f'step={step} speed={speed} acc={acc} start={start}')
     setup_port(opts.port)
-    enable_port(tic)
     tic.set32(set_starting_speed, start)
     tic.set32(set_max_speed, speed)
     tic.set32(set_max_accel, acc)
+    motor_info(tic, f'port={opts.port}')
+    enable_port(tic)
     time.sleep(0.5)
     tic.step(step)
     sys.exit()
